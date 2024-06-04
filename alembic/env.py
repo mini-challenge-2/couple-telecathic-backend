3657fdf5c242752e0351e7b6ed162e861228b5c4
ssabrut <michael.gunawan2002@gmail.com>
2024-06-04 09:35:37 +0700

    Add user migration

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.db.session import DATABASE_URL
+import os
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -13,6 +16,9 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+database_url = os.getenv('DATABASE_URL', DATABASE_URL)
+config.set_main_option('sqlalchemy.url', database_url)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
---
+++ alembic/versions/1adc49d550b6_create_users_table.py
@@ -0,0 +1,39 @@
+"""create users table
+
+Revision ID: 1adc49d550b6
+Revises: 
+Create Date: 2024-06-04 09:28:54.063281
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '1adc49d550b6'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+class Gender(sa.Enum):
+    female = 0
+    male = 1
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column('id', sa.String(8), primary_key=True, index=True, unique=True),
+        sa.Column('apple_id', sa.String(50), unique=True, nullable=False),
+        sa.Column('username', sa.String(20), unique=True, nullable=False),
+        sa.Column('gender', sa.Enum(Gender), nullable=False),
+        sa.Column('email', sa.String(50), unique=True, nullable=False),
+        sa.Column('birth', sa.Date, nullable=False),
+        sa.Column('latitude', sa.Float, nullable=False),
+        sa.Column('longitude', sa.Float, nullable=False),
+        sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.func.now()),
+    )
+
+def downgrade() -> None:
+    op.drop_table("users")
