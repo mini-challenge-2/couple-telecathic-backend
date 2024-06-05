d67c749bf45d7edfd786fbb118322ab3df2fc95f
ssabrut <michael.gunawan2002@gmail.com>
2024-06-05 10:42:58 +0700

    Add connection endpoint

--- app/api/v1/endpoint.py
+++ app/api/v1/endpoint.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.schemas.user import UserBase
-from app.schemas.user import User as UserSchema
+from app.schemas.connection import ConnectionBase
 from app.models.user import User as UserModel
+from app.models.connection import Connection
 from app.db import get_db
 from app.schemas import Response
 from app.utils.dependency import generate_uuid
@@ -13,7 +14,6 @@ router = APIRouter()
 @router.post('/register')
 async def register(user: UserBase, db: Session = Depends(get_db)):
     try:
-        pass
         __uuid = generate_uuid()
         if (db.query(UserModel).filter(UserModel.email == user.email).first()) or (db.query(UserModel).filter(UserModel.username == user.username).first()):
             return Response(status=400, message="User already exists")
@@ -34,5 +34,27 @@ async def register(user: UserBase, db: Session = Depends(get_db)):
         db.commit()
         db.refresh(db_user)
         return Response(status=201, message="User created successfully", data=db_user)
+    except Exception as e:
+        return Response(status=500, message=str(e))
+
+@router.post('/connect')
+async def connect(connection: ConnectionBase, db: Session = Depends(get_db)):
+    try:
+        user = db.query(UserModel).filter(UserModel.id == connection.user_id).first()
+        partner = db.query(UserModel).filter(UserModel.id == connection.partner_id).first()
+
+        if not user or not partner:
+            return Response(status=404, message="User not found")
+
+        data = {
+            "user_id": user.id,
+            "partner_id": partner.id,
+        }
+
+        db_connection = Connection(**data)
+        db.add(db_connection)
+        db.commit()
+        db.refresh(db_connection)
+        return Response(status=200, message="Connected successfully")
     except Exception as e:
         return Response(status=500, message=str(e))
