c493ab8b28fda26b8a06eb684da17af7dbe8067f
ssabrut <michael.gunawan2002@gmail.com>
2024-06-05 11:46:13 +0700

    Add connection service

--- app/api/v1/dependencies.py
+++ app/api/v1/dependencies.py
@@ -3,9 +3,17 @@ from sqlalchemy.orm import Session
 from app.db import get_db
 from app.repositories.user import UserRepository
 from app.services.user import UserService
+from app.repositories.connection import ConnectionRepository
+from app.services.connection import ConnectionService
 
 def get_user_repository(db: Session = Depends(get_db)):
     return UserRepository(db)
 
 def get_user_service(repository: UserRepository = Depends(get_user_repository)):
     return UserService(repository)
+
+def get_connection_repository(db: Session = Depends(get_db)):
+    return ConnectionRepository(db)
+
+def get_connection_service(repository: ConnectionRepository = Depends(get_connection_repository)):
+    return ConnectionService(repository)
