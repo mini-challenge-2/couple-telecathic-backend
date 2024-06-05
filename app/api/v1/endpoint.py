949db06da2847ac8f95af26fd71cce36cfc962ae
ssabrut <michael.gunawan2002@gmail.com>
2024-06-05 11:48:38 +0700

    Refactor connection /connect endpoint

--- app/api/v1/endpoint.py
+++ app/api/v1/endpoint.py
@@ -2,15 +2,15 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.schemas.user import UserBase
 from app.schemas.connection import ConnectionBase
-from app.models.user import User as UserModel
 from app.models.connection import Connection
 from app.db import get_db
 from app.schemas import Response
-from app.utils.dependency import generate_uuid
 from app.schemas.special_day import SpecialDayBase
 from app.models.special_day import SpecialDay
 from app.services.user import UserService
+from app.services.connection import ConnectionService
 from .dependencies import get_user_service
+from .dependencies import get_connection_service
 
 router = APIRouter()
 
@@ -23,24 +23,9 @@ async def create_user(user: UserBase, service: UserService = Depends(get_user_se
         return Response(status=500, message=str(e))
 
 @router.post('/connect')
-async def connect(connection: ConnectionBase, db: Session = Depends(get_db)):
+async def connect(connection: ConnectionBase, service: ConnectionService = Depends(get_connection_service)):
     try:
-        user = db.query(UserModel).filter(UserModel.id == connection.user_id).first()
-        partner = db.query(UserModel).filter(UserModel.id == connection.partner_id).first()
-
-        if not user or not partner:
-            return Response(status=404, message="User not found")
-
-        data = {
-            "user_id": user.id,
-            "partner_id": partner.id,
-        }
-
-        db_connection = Connection(**data)
-        db.add(db_connection)
-        db.commit()
-        db.refresh(db_connection)
-        return Response(status=200, message="Connected successfully")
+        return await service.create_connection(connection)
     except Exception as e:
         return Response(status=500, message=str(e))
 
