3cf0ee9223cd683e45958f187705064ced875a9c
ssabrut <michael.gunawan2002@gmail.com>
2024-06-06 09:39:55 +0700

    Add get interaction service and repository on v1 api dependencies

--- app/api/v1/dependencies.py
+++ app/api/v1/dependencies.py
@@ -5,6 +5,8 @@ from app.repositories.user import UserRepository
 from app.services.user import UserService
 from app.repositories.connection import ConnectionRepository
 from app.services.connection import ConnectionService
+from app.repositories.interaction import InteractionRepository
+from app.services.interaction import InteractionService
 
 def get_user_repository(db: Session = Depends(get_db)):
     return UserRepository(db)
@@ -17,3 +19,9 @@ def get_connection_repository(db: Session = Depends(get_db)):
 
 def get_connection_service(repository: ConnectionRepository = Depends(get_connection_repository)):
     return ConnectionService(repository)
+
+def get_interaction_repository(db: Session = Depends(get_db)):
+    return InteractionRepository(db)
+
+def get_interaction_service(repository: InteractionRepository = Depends(get_interaction_repository)):
+    return InteractionService(repository)
