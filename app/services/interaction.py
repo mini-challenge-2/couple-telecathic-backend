a4b5431d1fe1daa3d9a57f7bd931f9a6a6d9ee70
ssabrut <michael.gunawan2002@gmail.com>
2024-06-08 18:44:02 +0700

    Add send_interaction method in interaction service

--- app/services/interaction.py
+++ app/services/interaction.py
@@ -1,4 +1,5 @@
 from app.schemas.interaction import InteractionBase
+from app.schemas.interaction import NotificationRequest
 
 class InteractionService:
     def __init__(self, repository):
@@ -6,3 +7,6 @@ class InteractionService:
 
     async def create_interaction(self, interaction: InteractionBase):
         return await self.repository.create_interaction(interaction)
+
+    async def send_interaction(self, notification: NotificationRequest):
+        return await self.repository.send_interaction(notification)
